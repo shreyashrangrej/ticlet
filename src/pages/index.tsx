3cd2bab5c96a85ed4c97589a159ed3ef7b3e9c32
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-01-15 15:49:35 +0530

    installed tailwind and daisyui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,15 @@ import prisma from 'lib/prisma'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <h1 className="text-5xl font-bold">Welcome To TicLet</h1>
+          <p className="py-6">Get started by login into your Organization's account.</p>
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </div>
+    </div>
   )
 }
