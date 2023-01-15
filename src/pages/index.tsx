e7ee6b926ed3a0f77e3842eca0e8c49c68014cfe
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-01-15 15:35:25 +0530

    added prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import prisma from 'lib/prisma'
 
 const inter = Inter({ subsets: ['latin'] })
 
